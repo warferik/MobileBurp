688b524ff32a1a74fd1f7b352bf0e625f5a5e1e2
warferik <erik.warfield@gmail.com>
2020-07-20 08:16:26 -0600

    Update setupBurp.sh

--- setupBurp.sh
+++ setupBurp.sh
@@ -21,11 +21,8 @@ adb remount
 adb push $hash.0 /sdcard/
 
 echo "run following command in adb shell"
-echo 
-"mv /sdcard/$hash.0 /system/etc/security/cacerts/
-
-chmod 644 /system/etc/security/cacerts/$hash.0
-
-reboot"
+echo "mv /sdcard/$hash.0 /system/etc/security/cacerts/"
+echo "chmod 644 /system/etc/security/cacerts/$hash.0"
+echo "reboot"
 
 
